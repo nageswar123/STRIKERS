09d6cb2d1d46dd7d3ab86d74535e165fc42a3aca
nageswar123 <77968908+nageswar123@users.noreply.github.com>
2021-03-25 17:57:01 +0530

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-<!--connects the database with local host port number 27017-->
-mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
-    if (!err) { console.log('MongoDB Connection Succeeded.') }
-    else { console.log('Error in DB connection : ' + err) }
-});
-
-require('./employee.model');
